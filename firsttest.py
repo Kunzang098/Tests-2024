f1eae7c7d2978069430a4f7c4b67fc96ed5f4b6e
Kunzang098 <02230216.cst@rub.edu.bt>
2024-02-15 15:08:00 -0800

    changes to firsttest.py file

---
+++ firsttest.py
@@ -0,0 +1,1 @@
+print("I am as cool asmy progamme ICE ")
--- test-1.py
+++ test-1.py
@@ -1,2 +1,17 @@
-k= "Hello kity"
-print(k)
+#k= "Hello kity"
+#print(k)
+#print("Hello world")
+#print("I love programming")
+Grade=85
+if Grade >= 90:
+    print("Grade:A")
+elif Grade >= 80:
+    print("Grade:B")
+elif Grade >= 70:
+    print("Grade:C")  
+elif Grade >= 60:
+    print("Grade:D")  
+else:
+    print("Grade:F")  
+ 
+
