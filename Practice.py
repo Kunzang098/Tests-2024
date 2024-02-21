4bac26a44ab5d0f2101dbb466ecb2753c8d8c625
Kunzang098 <02230216.cst@rub.edu.bt>
2024-02-21 00:55:02 -0800

    First commit

---
+++ Practice.py
@@ -0,0 +1,7 @@
+# asking for input from the users.
+the_height = float(input("Enter the height in cm: "))
+the_weight = float(input("Enter the weight in kg: "))
+# defining a function for BMI.
+the_BMI = the_weight / (the_height/100)**2.
+# printing the BMI.
+print("Your Body Mass Index is", the_BMI)
