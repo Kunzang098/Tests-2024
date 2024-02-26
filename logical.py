f027362466eeb9e0b99db0ace788ed7b1761ab1a
Kunzang098 <02230216.cst@rub.edu.bt>
2024-02-26 13:47:42 -0800

    First commit

---
+++ logical.py
@@ -0,0 +1,15 @@
+#AND operator(EG)
+k=5
+print(k>3 and k<10)
+l=12
+print(l>10 and l%5==0)
+#OR operator ex
+x=5
+print(x<3 or x>10)
+y=12
+print(y<10 or y%2==0)
+#NOT operator ex
+x=5
+print(not(x>3 and x<10))
+y=12
+print(not(y>10 and y%5==0))
