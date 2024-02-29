fb0fc1c2ea0fcaca23e24e9a1ea1b5736ba450e6
Kunzang098 <02230216.cst@rub.edu.bt>
2024-02-29 13:53:52 -0800

    First commit c:; cd 'c:\Users\DELL\Desktop\Tests-2024'; & 'c:\Users\DELL\AppData\Local\Microsoft\WindowsApps\python3.11.exe' 'c:\Users\DELL\.vscode\extensions\ms-python.debugpy-2024.2.0-win32-x64\bundled\libs\debugpy\adapter/../..\debugpy\launcher' '61588' '--' 'C:\Users\DELL\Desktop\Tests-2024\Activity.py'

---
+++ Activity.py
@@ -0,0 +1,7 @@
+name=input("Enter your name:")
+print("Hello dear ",name)
+num1=float(input("Enter a number:"))
+num2=float(input("Enter a number:"))
+product=num1*num2
+print("The product is:",product)
+
