177875e706d54d7817d03fb5a12298cd10cb7d6f
Kunzang098 <02230216.cst@rub.edu.bt>
2024-02-29 13:57:13 -0800

    First commit

--- Activity.py
+++ Activity.py
@@ -4,4 +4,9 @@ num1=float(input("Enter a number:"))
 num2=float(input("Enter a number:"))
 product=num1*num2
 print("The product is:",product)
-
+sum=num1+num2
+print("The sum is:",sum)
+difference=num1-num2
+print("The difference is:",difference)
+quotient=num1/num2
+print("The quotient is:",quotient)
