bc73c3568a2780d4df0b743690b098daf9b3238a
KUNZANG LHAMO <02230216.cst@rub.edu.bt>
2024-04-25 10:57:06 +0530

    First commit

---
+++ classwork.py
@@ -0,0 +1,17 @@
+class dog:
+     def __init__(self,name,color,age):
+          self.name= name
+          self.color=color
+          self.age= age
+Dawa =dog("Dawa","Brown",4)  
+Nima =dog("Nima","Yellow",3)
+Blacky =dog("Blacky","Black",1)
+
+print(Dawa.age, Dawa.color,Dawa.name)
+print(Nima.age, Nima.color,Nima.name)
+
+print(Blacky.age, Blacky.color,Blacky.name)
+
+
+       
+                           
